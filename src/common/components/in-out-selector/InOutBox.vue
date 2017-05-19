96789204577951f5c265285ddb7bd4c212fcbae4
Mario Medrano <mariomedranonrd@gmail.com>
2017-05-18 22:44:33 -0400

    Check All changes

--- src/common/components/in-out-selector/InOutBox.vue
+++ src/common/components/in-out-selector/InOutBox.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="in-out-box">        
-        <input type="checkbox" v-model="checkAll">
+        <input type="checkbox" v-model="checkAll" @change="checkUnCheckAll" :disabled="!list.length">
         <input type="text" placeholder="Search" v-model="searchText">
         <!--add ket events to the list-->
         <!--as well add sort functionality-->
@@ -31,33 +31,33 @@
         },
         data() {
             return {
-                searchText: "",
-                checkAll: false
+                searchText: ""
             }
         },
         methods: {
             clear() {
                 // BEST WAY TO CLEAN ARRAY IN VUE (DOES NOT AFFECT THE COMPUTED)
                 this.value.splice(0, this.value.length);
+            },
+            checkUnCheckAll(e) {
+                var me = this;
+                me.clear();
+                if (e.target.checked)
+                    for (var i = 0; i < me.list.length; i += 1) {
+                        var item = me.list[i];
+                        me.value.push(item[me.valueField]);
+                    }     
             }
         },
         components: {
             "in-out-card": InOutCard
         },
-        // computed: {
-        //     checkAll() {
-        //         return this.list.length === this.value.length; // que sean mayor a cero
-        //     }
-        // },
-        watch: {
-            checkAll(newVal, oldVal) {
-                this.clear();
-                if (newVal)
-                    for (var i = 0; i < this.list.length; i += 1) {
-                        var item = this.list[i];
-                        this.value.push(item[this.valueField]);
-                    }                
-            },
+        computed: {
+            checkAll() {
+                return this.list.length > 0 && this.list.length === this.value.length;
+            }
+        },
+        watch: {           
             searchText(newVal, oldVal) {
                 this.clear();
             }
